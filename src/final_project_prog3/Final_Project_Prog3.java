b80e9a9216d55aa2a6dc119b5b0625ee74d7faeb
abdulrahman2210 <121126032+abdulrahman2210@users.noreply.github.com>
2023-08-04 21:20:12 +0300

    first commit

---
+++ src/final_project_prog3/Final_Project_Prog3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package final_project_prog3;
+
+import Controller.Admin.LoginAdminController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Final_Project_Prog3 extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/HomePage/HomePage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
